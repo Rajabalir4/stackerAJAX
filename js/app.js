bdfa9b96c2b943fb5874fddc735e369b3b485434
RajabaliR4 <Rajabalir4@gmail.com>
2019-06-17 23:48:49 +0530

    get inspired feature added

--- js/app.js
+++ js/app.js
@@ -31,6 +31,28 @@ var showQuestion = function(question) {
 	return result;
 };
 
+function showAnswerers(answerer){
+	var result = $('.templates .wrapper').clone();
+	//setting the link
+		
+	var resultElem = result.find('#mylink');
+	resultElem.attr('href', answerer.user.link);
+
+	//set the profile picture
+	var profile = result.find('.profile img');
+	profile.attr('src', answerer.user.profile_image);
+
+	//set the title description
+	var description = result.find('.description .title');
+	description.text(answerer.user.display_name+' ( '+answerer.user.user_type+" ) ");
+
+	//set the reputationScore
+	var reputationScore = result.find('.description .reputationscore');
+	reputationScore.text(answerer.user.reputation);
+
+	return result;
+};
+
 
 // this function takes the results object from StackOverflow
 // and returns the number of results and tags to be appended to DOM
@@ -45,6 +67,7 @@ var showError = function(error){
 	var errorText = '<p>' + error + '</p>';
 	errorElem.append(errorText);
 };
+
 
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
@@ -81,6 +104,40 @@ var getUnanswered = function(tags) {
 	});
 };
 
+// this function gets the list from stack api
+function getAnswerer(tag){
+	console.log(tag);
+	var request ={
+		site:'stackoverflow',
+		order: 'desc',
+		sort: 'reputation'
+	};
+
+	$.ajax({
+		url: 'http://api.stackexchange.com/2.2/tags/'+tag+'/top-answerers/all_time',
+		type: 'GET',
+		dataType: 'jsonp',
+		data: request,
+	})
+	.done(function(result) {
+		//search results
+		var searchResults = showSearchResults(tag, result.items.length);
+		//append the search results		
+		$('.search-results').html(searchResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(i, item) {
+			
+			var answerers = showAnswerers(item);
+			$('.results').append(answerers);
+		});
+	})
+	.fail(function() {
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
 
 $(document).ready( function() {
 	$('.unanswered-getter').submit( function(e){
@@ -90,5 +147,12 @@ $(document).ready( function() {
 		// get the value of the tags the user submitted
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
+	});
+	$('.inspiration-getter').submit(function(event) {
+		event.preventDefault();
+
+		$('.results').html('');
+		var tags = $(this).find("input[name='answerers']").val();
+		getAnswerer(tags);
 	});
 });
